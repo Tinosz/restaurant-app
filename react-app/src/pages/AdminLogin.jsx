5ac3bf8bb97695353c374e413959a467150792dc
haruza123 <haruzakatana@gmail.com>
2023-10-21 13:39:49 +0700

    Update AdminLogin.jsx

--- react-app/src/pages/AdminLogin.jsx
+++ react-app/src/pages/AdminLogin.jsx
@@ -2,71 +2,140 @@ import adminAxiosClient from "../axios-admin-client";
 import { useRef, useState } from "react";
 import { useStateContext } from "../context/ContextProvider";
 
-export default function AdminLogin () {
-    const usernameRef = useRef();
-    const passwordRef = useRef();
+export default function AdminLogin() {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
 
-    const [errors, setErrors] = useState(null);
+  const [errors, setErrors] = useState(null);
 
-    const {setUser, setAdminToken} = useStateContext();
+  const { setUser, setAdminToken } = useStateContext();
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        const payload = {
-            username: usernameRef.current.value,
-            password: passwordRef.current.value,
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    };
+    setErrors(null);
+    adminAxiosClient
+      .post("adminlogin", payload)
+      .then(({ data }) => {
+        setUser(data.admin);
+        setAdminToken(data.token);
+      })
+      .catch((err) => {
+        const response = err.response;
+        if (response && response.status === 422) {
+          if (response.data.errors) {
+            setErrors(response.data.errors);
+          } else {
+            setErrors({
+              username: [response.data.message],
+            });
+          }
         }
-        setErrors(null)
-        adminAxiosClient.post('adminlogin', payload)
-        .then(({data}) => {
-            setUser(data.admin)
-            setAdminToken(data.token)
-        })
-        .catch(err => {
-            const response = err.response;
-            if (response && response.status === 422) {
-                if (response.data.errors) {
-                    setErrors(response.data.errors);
-                } else {
-                    setErrors({
-                        username: [response.data.message]
-                    })
-                }
-            }
-        })
-    }
+      });
+  }
 
-    return (
-        <>
-        <h1>Sign in to the Admin Account</h1>
+  const styles = {
+    pageContainer: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#957964",
+    },
+    loginBox: {
+      border: "none",
+      padding: "1%",
+      borderRadius: "8px",
+      width: "400px",
+      backgroundColor: "#5E1219",
+      textAlign: "left",
+      transition: "opacity 1s, transform 1s",
+    },
+    title: {
+      color: "#fff",
+    },
+    label: {
+      color: "#fff",
+    },
+    inputGroup: {
+      marginBottom: "10px",
+    },
+    input: {
+      width: "100%",
+      background: "transparent",
+      border: "1px solid #fff",
+      borderRadius: "4px",
+      padding: "10px",
+      color: "#fff",
+    },
+    button: {
+      backgroundColor: "rgba(255, 255, 255, 0.3)",
+      color: "#5E1219",
+      width: "100%",
+      borderRadius: "20px",
+    },
+    centeredText: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    text: {
+      color: "#fff",
+      marginTop: "10px",
+    },
+    link: {
+      color: "#fff",
+      transition: "color 0.3s",
+    },
+    hoverButton: {
+      cursor: "pointer",
+    },
+    hoverLink: {
+      cursor: "pointer",
+    },
+  };
+
+  return (
+    <div style={styles.pageContainer}>
+      <div style={styles.loginBox}>
+        <h1 style={styles.title}>Sign in to the Admin Account</h1>
         <form onSubmit={onSubmit}>
+          <div style={styles.inputGroup}>
+            <label style={styles.label}>Admin Username:</label>
+            {errors && errors.username && (
+              <p className="text-red-600">{errors.username[0]}</p>
+            )}
             <div>
-                <label>
-                    Admin Username:
-                </label>
-                {errors && errors.username && (
-                    <p className="text-red-600">{errors.username[0]}</p>
-                )}
-                <div>
-                    <input ref={usernameRef} placeholder="Enter the Admin Username"/>
-                </div>
+              <input
+                ref={usernameRef}
+                placeholder="Enter the Admin Username"
+                style={styles.input}
+              />
             </div>
+          </div>
+          <div style={styles.inputGroup}>
+            <label style={styles.label}>Password:</label>
             <div>
-                <label>
-                    Password
-                </label>
-                <div>
-                    <input ref={passwordRef} type="password" placeholder="Enter the Password"/>
-                </div>
+              <input
+                ref={passwordRef}
+                type="password"
+                placeholder="Enter the Password"
+                style={styles.input}
+              />
             </div>
-            <button>
-                Sign In
-            </button>
-            <p>
+          </div>
+          <button style={styles.button}>Sign In</button>
+          <p style={styles.text}>
             Are you a User?{" "}
-                <a style={{ margin: "10px 0" }} href="/Login">Log In here.</a>
-            </p>
+            <a style={styles.link} href="/Login">
+              Log In here.
+            </a>
+          </p>
         </form>
-        </>
-    )    
+      </div>
+    </div>
+  );
 }
