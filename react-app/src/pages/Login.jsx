a6e4028e7442cb35f3b90c7955010ba737aef473
haruza123 <haruzakatana@gmail.com>
2023-10-21 13:25:40 +0700

    Update Login.jsx

--- react-app/src/pages/Login.jsx
+++ react-app/src/pages/Login.jsx
@@ -1,10 +1,11 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useStateContext } from "../context/ContextProvider";
 import axiosClient from "../axios-client";
 
 export default function Login() {
   const loginRef = useRef();
   const passwordRef = useRef();
+  const [isVisible, setIsVisible] = useState(false); // Tambahkan state untuk mengendalikan animasi
 
   const [errors, setErrors] = useState(null);
 
@@ -38,10 +39,20 @@ export default function Login() {
       });
   };
 
+  // Setelah komponen dimuat, atur state isVisible ke true untuk memicu animasi fade-in
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
   return (
     <div style={styles.pageContainer}>
-      <div style={styles.loginBox}>
-        <h1 style={styles.title}>Sign in to your account</h1>
+      <div
+        style={{
+          ...styles.loginBox,
+          animation: isVisible ? "fadeInUp 1s" : "", // Animasi hanya akan berjalan jika isVisible adalah true
+        }}
+      >
+        <h1 style={styles.title}>Sign in to your account</h1><br></br>
         <form onSubmit={onSubmit}>
           <div style={styles.inputGroup}>
             <label style={styles.label}>Email/Username:</label>
@@ -70,9 +81,20 @@ export default function Login() {
               />
             </div>
           </div>
-          <button style={styles.button}>Sign In</button>
-          <p style={{ ...styles.text, marginTop: "20px" }}>Don't have an account? <a href="/SignUp">Sign Up here.</a></p>
-          <p style={styles.text}>Are you an Admin? <a href="/AdminLogin">Log In here.</a></p>
+          <button style={{ ...styles.button, ...styles.hoverButton }}>Sign In</button>
+          <div style={styles.orLine}>
+            <div style={styles.orCircle}></div>
+            <div style={styles.orText}>OR</div>
+            <div style={styles.orCircle}></div>
+          </div>
+          <div style={styles.centeredText}>
+            <p style={styles.text}>
+              Don't have an account? <a href="/SignUp" style={{ ...styles.link, ...styles.hoverLink }}>Sign Up here.</a>
+            </p>
+            <p style={styles.text}>
+              Are you an Admin? <a href="/AdminLogin" style={{ ...styles.link, ...styles.hoverLink }}>Log In here.</a>
+            </p>
+          </div>
         </form>
       </div>
     </div>
@@ -85,15 +107,15 @@ const styles = {
     justifyContent: "center",
     alignItems: "center",
     height: "100vh",
-    backgroundColor: "#957964", // Latar belakang halaman
+    backgroundColor: "#957964",
   },
   loginBox: {
-    border: "none", // Menghilangkan border
-    padding: "3%",
+    border: "none",
+    padding: "1%",
     borderRadius: "8px",
-    width: "400px", // Lebih besar
-    backgroundColor: "rgba(94, 18, 25, 0.7)", // Transparan 30%
-    textAlign: "left", // Teks di kiri
+    width: "400px",
+    backgroundColor: "#5E1219",
+    textAlign: "left",
   },
   title: {
     color: "#fff",
@@ -107,13 +129,48 @@ const styles = {
   input: {
     width: "100%",
     background: "transparent",
+    border: "1px solid #fff",
+    borderRadius: "4px",
+    padding: "10px",
+    color: "#fff",
   },
   button: {
-    backgroundColor: "rgba(255, 255, 255, 0.3)", // Transparan 30%
+    backgroundColor: "rgba(255, 255, 255, 0.3)",
     color: "#5E1219",
+    width: "100%",
+    borderRadius: "20px",
+  },
+  orLine: {
+    display: "flex",
+    alignItems: "center",
+  },
+  orCircle: {
+    flex: "1",
+    borderBottom: "1px solid #fff",
+  },
+  orText: {
+    color: "#fff",
+    padding: "10px",
+  },
+  centeredText: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   text: {
     color: "#fff",
     marginTop: "10px",
   },
+  link: {
+    color: "#fff",
+    transition: "color 0.3s",
+  },
+  hoverButton: {
+    cursor: "pointer",
+  },
+  hoverLink: {
+    cursor: "pointer",
+  },
+
+  
 };
